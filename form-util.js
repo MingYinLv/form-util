674cb1ca436a4f5e5704cb448412538902c45bf5
lvmingyin <lvmingyin@vip.qq.com>
2016-06-13 13:31:47 +0800

    formToJson

--- form-util.js
+++ form-util.js
@@ -1,19 +1,17 @@
 (function (root) {
     function formUtil() {
-        var util = {
-            "mobile": /^1[3|4|5|7|8]\d{9}$/,
-            "realName": "",
-            "null": function (value) {
-                if (this.trim(value).length === 0) {
+        //表单验证
+        var validate = {
+            'mobile': /^1[3|4|5|7|8]\d{9}$/,
+            'realName': '',
+            'null': function (value) {
+                if (trim(value).length === 0) {
                     return false;
                 }
                 return true;
             },
-            "trim": function (value) {
-                return value.replace(/^(\s|\u00A0)+/, '').replace(/(\s|\u00A0)+$/, '');
-            },
-            "email": /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/,
-            "IDCard": function (idCard) {
+            'email': /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/,
+            'IDCard': function (idCard) {
                 var regIdCard = /^(^[1-9]\d{7}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}$)|(^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])((\d{4})|\d{3}[Xx])$)$/;
                 if (regIdCard.test(idCard)) {
                     if (idCard.length == 18) {
@@ -26,7 +24,7 @@
                         var idCardMod = idCardWiSum % 11;
                         var idCardLast = idCard.substring(17);
                         if (idCardMod == 2) {
-                            if (idCardLast == "X" || idCardLast == "x") {
+                            if (idCardLast == 'X' || idCardLast == 'x') {
 
                                 return true;
                             } else {
@@ -45,21 +43,72 @@
                     return false;
                 }
             },
-            "bankID": /^(\d{16}|\d{19})$/,
-            "password": /^[\da-zA-Z]{6,18}$/,
-            "verify": function (pat, value, callback) {
-                var _ = this;
-                if (typeof _[pat] === "object" && _[pat].test) {
-                    return typeof callback === "function" && callback(_[pat].test(value));
-                } else if (typeof _[pat] === "function") {
-                    return typeof callback === "function" && callback(_[pat]());
+            'bankID': /^(\d{16}|\d{19})$/,
+            'password': /^[\da-zA-Z]{6,18}$/,
+            'verify': function (rule, value, callback) {
+                var _ = this, _verifyResult = false;
+                if (typeof _[rule] === 'object' && _[rule].test) {
+                    _verifyResult = _[rule].test(value);
+                } else if (typeof _[rule] === 'function') {
+                    _verifyResult = _[rule]();
                 }
+                typeof callback === 'function' && callback(_verifyResult);
+                return _verifyResult;
             }
         };
-        return {
-            "verity": function (pat, value, callback) {
-                return util.verify(pat, value, callback);
+        //去除空格
+        var trim = function (value) {
+            return value.replace(/^(\s|\u00A0)+/, '').replace(/(\s|\u00A0)+$/, '');
+        };
+        /**
+         * 将form转换为JSON对象
+         * @param formDom form的dom对象
+         */
+        var formToJSON = function (formDom) {
+            var _arr = [], _obj = {}, _name, _temp, _value;
+
+            _arr = _arr.concat(Array.prototype.slice.apply(formDom.getElementsByTagName('input')));
+            _arr = _arr.concat(Array.prototype.slice.apply(formDom.getElementsByTagName('select')));
+            _arr = _arr.concat(Array.prototype.slice.apply(formDom.getElementsByTagName('textarea')));
+            for (var i = 0, len = _arr.length; i < len; i++) {
+                _temp = _arr[i];
+                _name = _temp.getAttribute('name');
+                _value = _temp.getAttribute('value');
+                if (_name && !_obj[_name]) {
+                    if (_temp.checked || (_temp.getAttribute('type') !== 'checkbox' && _temp.getAttribute('type') !== 'radio')) {
+                        _obj[_name] = _value;
+                    }
+                } else if (_name) {
+                    if (_temp.checked || (_temp.getAttribute('type') !== 'checkbox' && _temp.getAttribute('type') !== 'radio')) {
+                        if (typeof _obj[_name] === 'string'){
+                            _obj[_name] = [_obj[_name]];
+                        }
+                        _obj[_name].push(_value);
+                    }
+                }
             }
+            return _obj;
+        };
+        return {
+            /**
+             *
+             * @param rule 验证规则
+             * @param value 验证值
+             * @param callback 回调函数
+             * @returns Boolean
+             */
+            'verity': function (rule, value, callback) {
+                return validate.verify(rule, value, callback);
+            },
+            /**
+             * 修改（添加）验证规则
+             * @param ruleName 名称
+             * @param rule 规则
+             */
+            'modifyVerity': function (ruleName, rule) {
+                validate[ruleName] = rule;
+            },
+            'formToJSON': formToJSON
         }
     };
 
